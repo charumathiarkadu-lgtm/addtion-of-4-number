dc957a29b0baa081be245ae151ecaa7e822ae764
charumathiarkadu-lgtm <charumathiarkadu@gmail.com>
2025-12-07 22:59:12 +0530

    Add files via upload

---
+++ sum1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d;
+    printf("enter the number:");
+    scanf("%d",&a);
+    printf("enter number:");
+    scanf("%d",&b);
+    printf("enter the number:");
+    scanf("%d",&c);
+    printf("enter number:");
+    scanf("%d",&d);
+    printf("sum=%d",a+b+c+d);
+    return 0;
+}
